2d2496a795cdda8472b1ac0f2d88658c8460d00d
Mel hsieh <mehsieh89@gmail.com>
2018-11-20 20:30:41 -0800

    Updates

---
+++ precache-manifest.82cdd13eb346aa2dfe85474a1193f2ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e35fa951ef3776c42df",
+    "url": "/sleeper/static/css/main.a1315754.chunk.css"
+  },
+  {
+    "revision": "4e35fa951ef3776c42df",
+    "url": "/sleeper/static/js/main.4e35fa95.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/sleeper/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "9d9b5fe58055355e77db",
+    "url": "/sleeper/static/js/runtime~main.9d9b5fe5.js"
+  },
+  {
+    "revision": "d76f3546600971a9c551360bbdc01775",
+    "url": "/sleeper/index.html"
+  }
+];
